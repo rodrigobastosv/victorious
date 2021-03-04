35dc276d37c037a24409e36b079f5f3fed59c219
rodrigobastosv <rodrigobastosv@gmail.com>
2021-03-03 23:18:48 -0300

    ajustando videos page

--- lib/page/videos/videos_page.dart
+++ lib/page/videos/videos_page.dart
@@ -1,10 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_toggle_tab/flutter_toggle_tab.dart';
 import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 import '../shared/vs_loading.dart';
 import '../shared/vs_scaffold.dart';
 import '../shared/vs_title.dart';
+
+final categorias = [
+  'Todos',
+  'Diversos',
+  'Entrevistas',
+  'Eventos',
+];
 
 class VideosPage extends StatefulWidget {
   VideosPage({Key key}) : super(key: key);
@@ -14,6 +22,9 @@ class VideosPage extends StatefulWidget {
 }
 
 class _VideosPageState extends State<VideosPage> {
+  int selectedLabelIndex = 0;
+  List<QueryDocumentSnapshot> docs;
+
   @override
   Widget build(BuildContext context) {
     return VSScaffold(
@@ -23,38 +34,16 @@ class _VideosPageState extends State<VideosPage> {
         builder: (_, snapshot) {
           if (snapshot.hasData) {
             final data = snapshot.data;
-            final docs = data.docs;
+            docs = data.docs;
             return Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisSize: MainAxisSize.max,
               children: [
                 VSTitle('Vídeos'),
                 Expanded(
-                  child: GridView.builder(
-                    padding: EdgeInsets.symmetric(horizontal: 16),
-                    itemBuilder: (_, i) {
-                      final idVideo = YoutubePlayerController.convertUrlToId(
-                          docs[i].data()['video_url']);
-                      return YoutubePlayerIFrame(
-                        controller: YoutubePlayerController(
-                          initialVideoId: idVideo,
-                          params: YoutubePlayerParams(
-                            showFullscreenButton: true,
-                          ),
-                        ),
-                      );
-                    },
-                    itemCount: docs.length,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      childAspectRatio: 1.5,
-                      crossAxisCount: MediaQuery.of(context).size.width > 1024
-                          ? 3
-                          : MediaQuery.of(context).size.width >= 768
-                              ? 2
-                              : 1,
-                      crossAxisSpacing: 8,
-                      mainAxisSpacing: 8,
-                    ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: _buildVideos(docs),
                   ),
                 ),
                 SizedBox(height: 8),
@@ -64,6 +53,68 @@ class _VideosPageState extends State<VideosPage> {
             return VSLoading();
           }
         },
+      ),
+    );
+  }
+
+  Widget _buildVideos(List<QueryDocumentSnapshot> docs) {
+    return Column(
+      children: [
+        FlutterToggleTab(
+          borderRadius: 0,
+          width: MediaQuery.of(context).size.width > 600 ? 80 : 180,
+          height: 40,
+          initialIndex: selectedLabelIndex,
+          selectedTextStyle: TextStyle(
+            fontWeight: FontWeight.w700,
+          ),
+          unSelectedTextStyle: TextStyle(),
+          labels: categorias,
+          selectedLabelIndex: (index) {
+            setState(() {
+              selectedLabelIndex = index;
+            });
+          },
+        ),
+        SizedBox(height: 24),
+        _getVideos(docs),
+      ],
+    );
+  }
+
+  Widget _getVideos(List<QueryDocumentSnapshot> docs) {
+    final categoriaEscolhida = categorias[selectedLabelIndex];
+    final docsFiltered = selectedLabelIndex == 0
+        ? docs
+        : docs
+            .where((doc) => doc.data()['categoria'] == categoriaEscolhida)
+            .toList();
+    return Expanded(
+      child: GridView.builder(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        itemBuilder: (_, i) {
+          final idVideo = YoutubePlayerController.convertUrlToId(
+              docsFiltered[i].data()['video_url']);
+          return YoutubePlayerIFrame(
+            controller: YoutubePlayerController(
+              initialVideoId: idVideo,
+              params: YoutubePlayerParams(
+                showFullscreenButton: true,
+              ),
+            ),
+          );
+        },
+        itemCount: docsFiltered.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          childAspectRatio: 1.5,
+          crossAxisCount: MediaQuery.of(context).size.width > 1024
+              ? 3
+              : MediaQuery.of(context).size.width >= 768
+                  ? 2
+                  : 1,
+          crossAxisSpacing: 8,
+          mainAxisSpacing: 8,
+        ),
       ),
     );
   }
